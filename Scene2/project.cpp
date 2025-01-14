02dee5075de4329537272969d677f05401fce799
Meheraj Hasan <meherajhasan2002@gmail.com>
2025-01-14 11:21:21 +0600

    scene 2 update 2

--- Scene2/project.cpp
+++ Scene2/project.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 # define pi 3.1416
+int frameNumber = 0;
 
 // Initial values of variables
 bool day = true;
@@ -39,39 +40,38 @@ void drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3);
 void drawCircle(float x1, float y1, float radius);
 
 //sky elements
-void drawSky(); //O21
-void drawCloud(); 
-void drawSunMoon(); //O23
+void drawSky(); //O201
+void drawCloud();
+void drawSunMoon(); //O203
 void drawStar();
 void drawFireflies();
 void drawLightningFlash();
 
 //field elements
-void drawField(); //O22
-void drawHut1(); //O25
-void drawHut2(); //O26
-void drawHut3(); //O27
-void drawHut4(); //O28
-void drawHut5(); //O29
-void drawHut6(); //O30
-void drawRiver();
+void drawField(); //O202
+void drawHut1(); //O205
+void drawHut2(); //O206
+void drawHut3(); //O207
+void drawHut4(); //O208
+void drawHut5(); //O209
+void drawHut6(); //O210
+void drawRiver(); 
 void drawBoat();
 void drawMountains();
 void drawTrees();
-void drawWindMill();
+void drawWindMill(); //O211
 
 //weather elements
 void drawRain(); //O24
 
 //animations
 void updateCloud(int value);
-void updateRainCloud(int value);
-void updateRain(int value);
-void initializeRain();
+void updateRain(int value); //A201
+void initializeRain(); //A202
 void updateFireflies(int value);
 void updateLightningFlash(int value);
 void updateBoat(int value);
-void updateWindMill(int value);
+void updateWindMill(int value); //A203
 
 void mouse(int button, int state, int x, int y)
 {
@@ -145,7 +145,19 @@ void drawCircle(float cx, float cy, float r)
     glEnd();
 }
 
-void drawSky() // O21
+void drawCircleWM(double radius)
+{
+    int d;
+    glBegin(GL_POLYGON);
+    for (d = 0; d < 32; d++)
+    {
+        double angle = 2 * pi / 32 * d;
+        glVertex2d(radius * cos(angle), radius * sin(angle));
+    }
+    glEnd();
+}
+
+void drawSky() // O201
 {
     if (day)
         glColor3f(0.25, 0.95, 1.01);
@@ -154,7 +166,7 @@ void drawSky() // O21
     drawRectangle(-200.0, 28.75, 200.0, 27.67, 200.0, 200.0, -200.0, 200.0);
 }
 
-void drawField() //O22
+void drawField() //O202
 {
     if (day)
         glColor3ub(38, 109, 5);
@@ -163,7 +175,7 @@ void drawField() //O22
     drawRectangle(-200.0, -200.0, 200.0, -200.0, 200.0, 28.75, -200.0, 28.75);
 }
 
-void drawSunMoon() //O23
+void drawSunMoon() //O203
 {
     if (day)
         glColor3f(0.97, 0.91, 0.39);
@@ -172,20 +184,20 @@ void drawSunMoon() //O23
     drawCircle(-11.25, 144.58, 18.0);
 }
 
-void drawRain() //O24
+void drawRain() //O204
 {
     if (rain)
     {
         for (int i = 0; i < raindrops.size(); i++)
         {
-            glColor3ub(8, 119, 231); 
+            glColor3ub(8, 119, 231);
             // Adjusted length of raindrops for better visibility
             drawLines(raindrops[i].x, raindrops[i].y, raindrops[i].x, raindrops[i].y - 6.0);
         }
     }
 }
 
-void updateRain(int value)
+void updateRain(int value) //A201
 {
     if (rain)
     {
@@ -214,7 +226,7 @@ void updateRain(int value)
     glutTimerFunc(50, updateRain, 0); // Faster update for smoother animation
 }
 
-void initializeRain()
+void initializeRain() //A202
 {
     raindrops.clear(); // Clear existing raindrops
     for (int i = 0; i < maxRaindrops; i++) {
@@ -227,7 +239,7 @@ void initializeRain()
     }
 }
 
-void drawHut1() //O25
+void drawHut1() //O205
 {
     glColor3ub(255, 197, 62);
     drawRectangle(-183.89, 30.84, -144.28, 29.72, -116.35, 3.58, -157.64, 2.78);
@@ -243,7 +255,7 @@ void drawHut1() //O25
         glColor3ub(76, 5, 61);
     else
         glColor3f(0.97, 0.91, 0.39);
-    
+
     drawRectangle(-186.89, 8.39, -186.83, -0.56, -175.92, -1.47, -175.64, 7.49);
 
     //door
@@ -256,7 +268,7 @@ void drawHut1() //O25
     drawPolygon(-158.93, -15.72, -159.27, -17.55, -119.21, -13.74, -119.3, -12.92, -123.37, -12.16);
 }
 
-void drawHut2() //026
+void drawHut2() //0206
 {
     glColor3ub(255, 197, 62);
     drawRectangle(-69.72, 29.65, -93.58, 6.17, -59.01, 5.31, -36.81, 30.16);
@@ -282,7 +294,7 @@ void drawHut2() //026
     drawRectangle(-59.46, -12.7, -58.95, -16.83, -19.68, -10.85, -20.21, -7.48);
 }
 
-void drawHut3() //O27
+void drawHut3() //O207
 {
     glColor3ub(255, 197, 62);
     drawRectangle(-2.02, 34.97, -11.22, 27.15, 3.66, 26.88, 12.98, 35.52);
@@ -300,7 +312,7 @@ void drawHut3() //O27
     drawRectangle(8.8, 27.05, 9.02, 21.53, 12.69, 21.97, 12.43, 27.14);
 }
 
-void drawHut4() //028
+void drawHut4() //0208
 {
     glColor3ub(255, 197, 62);
     drawRectangle(55.26, 39.82, 46.65, 32.74, 73.32, 32.01, 65.99, 39.5);
@@ -316,7 +328,7 @@ void drawHut4() //028
     drawRectangle(58.05, 32.42, 58.05, 28.09, 60.76, 28.09, 60.66, 32.35);
 }
 
-void drawHut5() //029
+void drawHut5() //0209
 {
     glColor3ub(247, 141, 67);
     drawRectangle(95.43, 8.77, 95.0, -5.68, 127.94, -7.6, 127.68, 10.42);
@@ -338,7 +350,7 @@ void drawHut5() //029
     drawRectangle(148.09, 10.68, 148.26, 4.11, 153.24, 4.1, 153.04, 10.67);
 }
 
-void drawHut6() //O30
+void drawHut6() //O210
 {
     glColor3ub(247, 141, 67);
     drawRectangle(176.57, 24.16, 176.54, 19.61, 190.61, 19.65, 190.81, 24.25);
@@ -357,29 +369,80 @@ void drawHut6() //O30
     drawRectangle(180.53, 23.84, 180.53, 19.62, 183.16, 19.63, 183.01, 23.82);
 }
 
+void drawWindMill() //O211
+{
+    // Draw the pole
+    glColor3f(0.8f, 0.8f, 0.9f);
+    drawRectangle(-153.04, 28.62, -149.81, 28.62, -149.81, 100.62, -153.04, 100.62);
+
+    // Draw the circle at the top of the pole
+    glPushMatrix();
+    glTranslatef(-151.425, 100.62, 0.0);
+    glColor3f(0.8f, 0.8f, 0.9f);
+    drawCircleWM(5.0);
+    glPopMatrix();
+
+    // Draw the windmill blades (rotating around the top-center of the pole)
+    glPushMatrix();
+    glTranslatef(-151.425, 100.62, 0.0); // Top-center of the pole
+    glRotatef(frameNumber * 10, 0.0, 0.0, 1.0); // Rotate blades based on animation frame
+    glColor3f(0.4f, 0.4f, 0.8f);
+    for (int i = 0; i < 4; i++) // Fixed loop condition to iterate exactly 4 times
+    {
+        drawRectangle(-2.25, 4.76, -7.52, 24.04, -2.65, 40.23, 1.56, 24.45); // Adjusted blade coordinates relative to the rotation center
+        glRotatef(90, 0.0, 0.0, 1.0); // Rotate each blade by 90 degrees
+    }
+    glPopMatrix();
+}
+
+void updateWindMill(int value) //A203
+{
+    frameNumber++;
+    glutPostRedisplay(); // Request a redraw
+    glutTimerFunc(30, updateWindMill, 0); // 60 FPS
+}
+
+void drawRiver() //O212
+{
+    glColor3ub(8, 119, 231);
+    drawRectangle(-129.63, 13.2, -128.86, 7.1, -78.56, 7.1, -86.61, 13.03);
+    drawRectangle(-21.46, 2.59, -21.05, -5.12, -5.96, -8.26, 2.2, 0.7);
+    drawRectangle(-5.96, -8.26, 2.2, 0.7, 26.08, -1.68, 1.58, -11.52);
+    drawRectangle(1.58, -11.52, 26.08, -1.68, 42.21, -5.17, 6.37, -15.22);
+    drawTriangle(42.21, -5.17, 6.37, -15.22, 52.16, -6.53);
+    drawRectangle(52.16, -6.53, 6.37, -15.22, -4.45, -24.43, 65.77, -11.36);
+    drawTriangle(-4.45, -24.43, 65.77, -11.36, 78.35, -17.62);
+    drawTriangle(-4.45, -24.43, 78.35, -17.62, 63.67, -29.9);
+    drawRectangle(-4.45, -24.43, 63.67, -29.9, 62.82, -37.84, -51.07, -36.06);
+    drawRectangle(-51.07, -36.06, 62.82, -37.84, 80.0, -60.0, -35.19, -41.57);
+    drawTriangle(-35.19, -41.57, 80.0, -60.0, -18.49, -54.44);
+    drawRectangle(-20.95, -47.56, -18.49, -54.44, 83.05, -65.36, 80.0, -60.0);
+    drawTriangle(-18.49, -54.44, -36.17, -61.28, 83.05, -65.36);
+    drawRectangle(200.0, -60.0, -200.0, -60.0, -200.0, -200.0, 200.0, -200.0);
+}
+
 void scenery()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    drawSky();
+    drawSky(); //O201
     // drawCloud();
-    drawSunMoon();
+    drawSunMoon(); //O203
     // drawStar();
     // drawFireflies();
     // drawLightningFlash();
-    drawField();
-    if (rain) drawRain();
-    drawHut1(); //O25
-    drawHut2(); //O26
-    drawHut3(); //O27
-    drawHut4(); //O28
-    drawHut5(); //O29
-    drawHut6(); //O30
-    // drawPonds();
-    // drawRiver();
+    drawField(); //O202
+    if (rain) drawRain(); //O204
+    drawWindMill(); //O211
+    drawRiver(); //O212
+    drawHut1(); //O205
+    drawHut2(); //O206
+    drawHut3(); //O207
+    drawHut4(); //O208
+    drawHut5(); //O209
+    drawHut6(); //O210
     // drawBoat();
     // drawMountains();
     // drawTrees();
-    // drawWindMill();
     // drawRain();
     glutSwapBuffers();
 }
@@ -390,7 +453,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Season 2: Rainy Season in Rural Area");
+    glutCreateWindow("Rainy Season in Rural Area");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -401,11 +464,11 @@ int main(int argc, char** argv)
     glutKeyboardFunc(keyboard);
     // glutTimerFunc(80, updateCloud, 0);
     // glutTimerFunc(100, updateRainCloud, 0);
-    glutTimerFunc(100, updateRain, 0);
+    glutTimerFunc(100, updateRain, 0); //A201
     // glutTimerFunc(100, updateFireflies, 0);
     // glutTimerFunc(100, updateLightningFlash, 0);
     // glutTimerFunc(70, updateBoat, 0);
-    // glutTimerFunc(50, updateWindMill, 0);
+    glutTimerFunc(30, updateWindMill, 0); //A203
     glutMainLoop();
     return 0;
 }
